4476e25636d17b1ed30c82794b7c17b1c95bcfc2
Christopher M <chrism1389@gmail.com>
2022-01-28 21:09:12 -0800

    Add files via upload

---
+++ App25/App25/App25App.swift
@@ -0,0 +1,17 @@
+//
+//  App25App.swift
+//  App25
+//
+//  Created by Christopher on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct App25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App25/App25/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  App25
+//
+//  Created by Christopher on 1/28/22.
+//  https://www.creatisoft.com
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack{
+            
+            Image("Unknown")
+            
+            Text("Hello, world!")
+                .padding()
+            
+        }
+        
+     
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
